1a7b60627dee2d07ea234925fcbf1575a355741d
yylam-ai <yylam000@gmail.com>
2025-04-05 22:58:17 +0800

    fix dp classifier grid search

--- train.py
+++ train.py
@@ -17,7 +17,6 @@ os.environ["CUDA_VISIBLE_DEVICES"] = args['gpu']
 if not os.path.exists(os.path.join(os.getcwd(),'output', 'matrices')): os.makedirs(os.path.join(os.getcwd(),'output', 'matrices'))
 
 MODEL = ['SVM', 'DT', 'KNN', 'RF', 'MiniRocket', 'MultiRocket', 'dpClassifier', 'dpOptimalCNN']
-MODEL = ['dpOptimalCNN']
 REFIT= ['AUC']
 
 X_train = np.load(os.path.join(args['dataPath'], 'x_train_' + args['view'] + '.npy'))
